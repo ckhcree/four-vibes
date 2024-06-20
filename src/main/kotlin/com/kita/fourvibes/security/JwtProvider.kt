3b58da438b35f36fb1abc849cfbc2d1322447e1d
ckhcree <kbyo0324@gmail.com>
2024-06-20 14:54:00 +0900

    :pencil2: 불필요한 부분 삭제

--- src/main/kotlin/com/kita/fourvibes/security/JwtProvider.kt
+++ src/main/kotlin/com/kita/fourvibes/security/JwtProvider.kt
@@ -19,7 +19,7 @@ class JwtProvider(
     @Value("\${secret}") private val secret: String,
     @Value("\${expiration-hours}") private val expirationHours: Long,
 ) {
-    fun generateToken(subject: String, role: String, expirationPeriod: Duration): String {
+    fun generateToken(subject: String, expirationPeriod: Duration): String {
         val claims: Claims = Jwts.claims().build()
 
         val now = Instant.now()
